1627c5354653f20d8795af474386af3f81489fa7
VlaRu <dn210690nvs@gmail.com>
2024-05-02 16:42:42 +0200

    fix: delete unnessesary files

--- src/utilities/formatCurrency.ts
+++
@@ -1,7 +0,0 @@
-const CURRENCY_FORMATTER = new Intl.NumberFormat(undefined, {
-  currency: "USD", style: "currency"
-})
-
-export default function formatCurrency(number: number) {
-  return CURRENCY_FORMATTER.format(number)
-}
