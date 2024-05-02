f929f823a309e922b6527563c92fdca19055d5d8
VlaRu <dn210690nvs@gmail.com>
2024-05-02 16:40:54 +0200

    feat: add build files

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
+  mode: "development",
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
